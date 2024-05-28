104b7280bf484826e69ea2523315d4e282ad483f
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-05-28 12:25:33 -0400

    new world

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from "react";
+import ArticleData from "./Components/ArticlesData";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-5xl font-bold underline">Hello world!</h1>{" "}
+      <ArticleData />
+    </>
+  );
+}
+
+export default App;
