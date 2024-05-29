e22621c2b03e50469d19fbf32091d4ec0afa435d
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-05-29 00:40:25 -0400

    updating navbar

--- src/Components/Navigation.tsx
+++ src/Components/Navigation.tsx
@@ -17,13 +17,28 @@ const Navbar: React.FC = () => {
           </Link>
         </div>
 
-        <div className="flex items-start space-x-4">
+        <div className="flex items-start space-x-4 mt-3">
           <Link to="/articles">
-            <button className="text-white font-bold">Articles</button>
+            <button className="text-white font-bold">ARTICLES</button>
           </Link>
           <div className="relative">
-            <button className="text-white font-bold" onClick={toggleDropdown}>
-              Categories
+            <button className="text-white font-bold flex items-center" onClick={toggleDropdown}>
+              CATEGORIES<svg
+                className={`w-4 h-4 ml-1 transition-transform ${
+                  isDropdownOpen ? "transform rotate-180" : ""
+                }`}
+                fill="none"
+                stroke="#FF3D00"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M19 9l-7 7-7-7"
+                ></path>
+              </svg>
             </button>
             {isDropdownOpen && (
               <div className="absolute right-0 mt-2 w-48 bg-white rounded-md shadow-lg py-2">
