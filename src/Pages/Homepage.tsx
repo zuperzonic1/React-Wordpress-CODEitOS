0ca6c2ef28b3b5a4f4164ab143c6e096b199b8c8
Micheal Felobes <65277151+zuperzonic1@users.noreply.github.com>
2024-07-20 16:25:49 -0400

    changing top picks

--- src/Pages/Homepage.tsx
+++ src/Pages/Homepage.tsx
@@ -9,10 +9,10 @@ const Homepage = ({ articles }: { articles: Article[] }) => {
         <h1 className="text-4xl font-bold">TOP PICKS</h1>
       </header>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
+        <CustomCard articleId={115} articles={articles} />
         <CustomCard articleId={52} articles={articles} />
         <CustomCard articleId={108} articles={articles} />
         <CustomCard articleId={64} articles={articles} />
-        <CustomCard articleId={77} articles={articles} />
       </div>
       <div className="shadow mb-7 mt-10 rounded-lg">
         <h1 className="text-3xl text-white font-bold">RECENTLY ADDED</h1>
